2efe9f32a6e9f0f6e773ebfd1f6e47df810a804c
AdisufatulAnam <203310019@students.akakom.ac.id>
2022-10-21 21:32:50 +0700

    coba

---
+++ ECMAScript6/array.js
@@ -0,0 +1,9 @@
+const favorites = ['Seafood', 'Salad', 'Nugget', 'Soup'];
+const coba=['salak','tape'];
+const [firstFood, secondFood, thirdFood, fourthFood] = favorites;
+ 
+// console.log(firstFood);
+// console.log(secondFood);
+// console.log(thirdFood);
+// console.log(fourthFood);
+console.log(favorites);
