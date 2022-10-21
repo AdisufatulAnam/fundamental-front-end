e6d16c3b063bdb232f9333bbada18cc73b28fec5
AdisufatulAnam <203310019@students.akakom.ac.id>
2022-10-21 20:50:49 +0700

    belajar fundamental front end

---
+++ ECMAScript6/main.js
@@ -0,0 +1,11 @@
+const user = {
+  firstName: "Dimas",
+  lastName: "Saputra",
+  age: 18,
+  
+}
+ const {firstName,lastName,age}=user;
+ 
+ //dipanggilmengukana consol.log
+  
+ console.log(firstName,age);
