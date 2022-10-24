b39d8eef3abc90359c9748ab97e986476ff94c82
AdisufatulAnam <203310019@students.akakom.ac.id>
2022-10-25 00:37:35 +0700

    update setTimeout.js

--- ECMAScript6/setTimeout.js
+++ ECMAScript6/setTimeout.js
@@ -1,6 +1,29 @@
-console.log('Selamat Datang');
-setTimeout(()=>{
-    console.log('terima kasih sudah mampir ,silahkan datang kembali')
+// console.log('Selamat Datang');
+// setTimeout(()=>{
+//     console.log('terima kasih sudah mampir ,silahkan datang kembali')
 
-},9000);
-console.log('ada  yang bisa kami bantu ?');
+// },9000);
+// console.log('ada  yang bisa kami bantu ?');
+
+// const getCake = () => {
+//     let cake = null;
+//     console.log('Sedang membuat kue, silakan tunggu ....');
+    
+//     setTimeout(() => {
+//       cake = 'Kue Selesai!';
+//     }, 3000);
+    
+//     return cake;
+//   };
+   
+//   const cake = getCake();
+//   console.log(cake);
+   
+const getCake= callback=>{
+    let cake=null;
+    console.log('sedang membut kue, silahkan tunggu....');
+    setTimeout(()=>{
+        cake='kue selesai';
+    },3000);
+    return cake;
+}
