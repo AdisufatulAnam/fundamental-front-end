4628d1bf4c8e375dce77447470216533e2055ff2
AdisufatulAnam <203310019@students.akakom.ac.id>
2022-10-26 00:23:16 +0700

    update

--- ECMAScript6/promise.js
+++ ECMAScript6/promise.js
@@ -1,11 +1,36 @@
+// const exsekutorFunction=(resolve, reject)=>{
+//     const isCoffeeMakerReady=true;
+    
+//     if(isCoffeeMakerReady){
+//         resolve('kopi berhasil dibuat');
+//     }else{
+//         reject('mesin kopi tidak bisa digunakan');
+//     }
+// };
+// const makeCoffee=new Promise(exsekutorFunction);
+// console.log(makeCoffee);
+
+//funsi onfulfiled dan onreject
 const exsekutorFunction=(resolve, reject)=>{
     const isCoffeeMakerReady=false;
-    
-    if(isCoffeeMakerReady){
-        resolve('kopi berhasil dibuat');
+    if (isCoffeeMakerReady) {
+        resolve('kopi berhasil dibuat');    
     }else{
-        reject('mesin kopi tidak bisa digunakan');
+        reject('mesin kopi tidak bisa digunakan !');
     }
 };
+const handlerSuccess=resolvedValue=>{
+    console.log(resolvedValue);
+};
+const handlerRejected=rejectionReason=>{
+    console.log(rejectionReason);
+};
+// const makeCoffee=new Promise(exsekutorFunction);
+// makeCoffee.then(handlerSuccess,handlerRejected);
+
+
+// onRejeck mengunakan catch method
 const makeCoffee=new Promise(exsekutorFunction);
-console.log(makeCoffee);
+makeCoffee
+.then(handlerSuccess)
+.then(null,handlerRejected);
